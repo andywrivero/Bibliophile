043a0f6beaf784ba5af70eb4902f3b216103d22d
Sbarkleyiv <44170589+Sbarkleyiv@users.noreply.github.com>
2018-12-05 15:36:14 -0500

    Add files via upload

---
+++ BibliophileApplication/Others/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BibliophileApplication.Others
+{
+    public class DelegateCommand : ICommand
+    {
+        public Action<object> _execute;
+        public Predicate<object> _canexecute;
+
+        public DelegateCommand(Action<object> execute) : this(execute, null) { }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canexecute)
+        {
+            _execute = execute ?? throw new NullReferenceException("Execute delegate null refernce exception");
+            _canexecute = canexecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canexecute == null ? true : _canexecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; } 
+        }
+    }
+}
